5ed39d594a8d349d0275ec1d40adc1f81bea44c4
studios <ceobobby@yahoo.com>
2013-07-18 09:15:49 +0200

    Update web.js

--- web.js
+++ web.js
@@ -1,11 +1,12 @@
-
 var express = require('express');
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-var fs = require('fs');
-var content = fs.readFileSync('index.html', 'utf8');
+        var fs = require('fs');
+        var content = fs.readFileSync('index.html', 'utf8');
+        response.send(buf.toString());
 });
+
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
